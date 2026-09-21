4a1ebacfa4cb8190bdd99b9eeba8802f6da54fef
WhiteWidow228 <lein1337228@gmail.com>
2026-09-21 15:32:05 +0300

    Initial Docker Node Postgres setup

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+
+app.get("/", (req, res) => {
+    res.send("Backend works!");
+});
+
+
+app.get("/users", async (req, res) => {
+    try {
+        const result = await pool.query(
+            "SELECT * FROM users"
+        );
+
+        res.json(result.rows);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Database error");
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
